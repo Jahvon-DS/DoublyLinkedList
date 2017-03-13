fb8786123ea335f9934a88866548d4e6b4dc533a
Jahvon-DS <tvkids8@aol.com>
2017-03-12 22:42:11 -0400

    continuation of the assignment

--- LinkedList.java
+++ LinkedList.java
@@ -2,4 +2,29 @@ package listListHomework;
 
 public class LinkedList {
 
+	static Node head = null; 
+	
+	class Node{
+		int data;
+		Node next,prev; 
+		
+		Node(int d){
+			data = d; 
+			next = prev = null; 
+			
+		}
+	}
+	// creating the doubly linked list 
+	list.push(head, 2);
+	lsit.push(head, 4);
+	list.push(head, 8); 
+	list.push(head, 10);
+	
+	System.out.println("Original Linked List");
+	list.printList(head);
+	
+	void delete Node(Node head_ref, Node del){
+		
+		if 
+	}
 }
